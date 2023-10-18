59ed804edd6271f426833a3aab4942de265e0b23
Christiaan Bloemendaal <github@tnrd.net>
2023-10-18 22:37:00 +0200

    feat: add logging to seq

--- Program.cs
+++ Program.cs
@@ -3,23 +3,27 @@ using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 
-namespace TNRD.Zeepkist.GTR.Auth
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace TNRD.Zeepkist.GTR.Auth;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, configuration) =>
-                {
-                    configuration
-                        .Enrich.WithProperty("Application", "Zeepkist.GTR.Auth")
-                        .WriteTo.Console(LogEventLevel.Debug);
-                })
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, configuration) =>
+            {
+                configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("Source", "Auth")
+                    .MinimumLevel.Debug()
+                    .WriteTo.Seq(context.Configuration["Seq:Url"], apiKey: context.Configuration["Seq:Key"])
+                    .WriteTo.Console(LogEventLevel.Debug);
+            })
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
